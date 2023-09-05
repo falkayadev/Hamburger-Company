5c5c1538eab0d1e1e0b88ce9974424e156c3e05f
Furkan Alkaya <falkaya.dev@gmail.com>
2023-09-05 10:46:10 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.{html,js}',
+    './node_modules/tw-elements/dist/js/**/*.js'],
+  theme: {
+    extend: {
+      colors: {
+        custom: {
+          //1: "#9A3B3B",
+          1: "#6C91BF",
+          2: "#C08261",
+          3: "#E2C799",
+          4: "#F2ECBE",
+          5: "#3a3e40",
+        },
+        
+      },
+      fontFamily: {
+        logoFont: ["Pacifico", "cursive"],
+        mainFont: ["Poppins", "sans-serif"],
+      }
+    },
+    
+  },
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+}
+
